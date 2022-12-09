59ee11a0c4bf229ba93c9031d6a3d6af861659e2
ealdrich <lanndrich@gmail.com>
2022-12-09 15:38:49 -0800

    Part 2.1: JwtTokenGenerator

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,10 +1,33 @@
 namespace BuberDinner.Application.Services.Authentication;
+using BuberDinner.Application.Common.Interfaces.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
-        return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, "token");
+        // Check if user already exists
+
+        // Create user (generate unique ID)
+        Guid userId = Guid.NewGuid();
+
+        // Create JWT token
+        var token = _jwtTokenGenerator.GenerateToken(
+            userId,
+            firstName,
+            lastName);
+
+        return new AuthenticationResult(userId,
+            firstName,
+            lastName,
+            email,
+            token);
     }
     public AuthenticationResult Login(string email, string password)
     {
--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Infrastructure.Authentication;
 
 namespace BuberDinner.Infrastructure;
 
@@ -7,6 +9,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         // services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
 
         return services;
     }
