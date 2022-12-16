10c713ff8354541404f1e77d5e833f28fca4fb11
ealdrich <lanndrich@gmail.com>
2022-12-16 15:01:52 -0800

    Part 8.1:  Validation Behavior with MediatR.

--- BuberDinner.Application/DependencyInjection.cs
+++ BuberDinner.Application/DependencyInjection.cs
@@ -1,7 +1,14 @@
-// using BuberDinner.Application.Services.Authentication.Commands;
-// using BuberDinner.Application.Services.Authentication.Queries;
 using Microsoft.Extensions.DependencyInjection;
+
+using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Common.Behaviors;
+
+using ErrorOr;
+
 using MediatR;
+using FluentValidation;
+using System.Reflection;
 
 namespace BuberDinner.Application;
 
@@ -9,9 +16,21 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        // services.AddScoped<IAuthenticationCommandService, AuthenticationCommandService>();
-        // services.AddScoped<IAuthenticationQueryService, AuthenticationQueryService>();
         services.AddMediatR(typeof(DependencyInjection).Assembly);
+
+        services.AddScoped(
+            typeof(IPipelineBehavior<,>),
+            typeof(ValidationBehavior<,>));
+
+        // services.AddScoped<
+        //     IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>,
+        //     ValidateRegisterCommandBehavior>();
+
+        // services.AddScoped<
+        //     IValidator<RegisterCommand>,
+        //     RegisterCommandValidator>();
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         return services;
     }
