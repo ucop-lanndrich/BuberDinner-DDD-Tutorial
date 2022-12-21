72dda87d74d6e1a0e834c3de7d1773fae6e15090
ealdrich <lanndrich@gmail.com>
2022-12-20 17:08:48 -0800

    Part 12: Abstract base classes

---
+++ BuberDinner.Domain/Models/Entity.cs
@@ -0,0 +1,57 @@
+namespace BuberDinner.Domain.Models;
+
+public abstract class Entity<TId> : IEquatable<Entity<TId>>
+    where TId : notnull
+{
+    public TId Id { get; protected set; }
+
+    protected Entity(TId id)
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Entity<TId> other && Equals(other);
+    }
+
+    public bool Equals(Entity<TId>? other)
+    {
+        return Equals((object?)other);
+    }
+
+    public static bool operator ==(Entity<TId>? a, Entity<TId>? b)
+    {
+        if (a is null && b is null)
+        {
+            return true;
+        }
+
+        if (a is null || b is null)
+        {
+            return false;
+        }
+
+        return Equals(a, b);
+    }
+
+    public static bool operator !=(Entity<TId>? a, Entity<TId>? b)
+    {
+        if (a is null && b is null)
+        {
+            return true;
+        }
+
+        if (a is null || b is null)
+        {
+            return false;
+        }
+
+        return Equals(a, b);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
